2ae58d66cf17edd07def1f83ad4a1e5ff060d458
michascant <89426143+MichaScant@users.noreply.github.com>
2025-03-26 21:48:41 -0400

    updated the scraping to better identify the type of language the file is using

--- Q1/git_repo_stats.py
+++ Q1/git_repo_stats.py
@@ -26,34 +26,43 @@ from scrapy.signalmanager import dispatcher
 load_dotenv()
 
 class GithubSpider(scrapy.Spider):
+    
     name = 'github_spider'
     
     def __init__(self, *args, **kwargs):
         super(GithubSpider, self).__init__(*args, **kwargs)
-        self.processed_languages_extensions = {}
+        self.valid_language_names = set()
+        self.extensions_to_language_names = set()
         self.processed_languages_extensions_count = {}
 
     def start_requests(self):
-
+        token = os.environ.get('GITHUB_TOKEN')
+        self.headers = {
+            'Authorization': f'token {token}',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br'
+        }
+    
         #TODO: remove the extension if come across a second time
         languages = yaml.safe_load(open('languages.yml'))
         for key, value in languages.items():
             if (value['type'] == 'programming' and 'extensions' in value):
+                self.valid_language_names.add(key)
                 for extension in value['extensions']:
-                    self.processed_languages_extensions[extension] = key
-               
+                    self.extensions_to_language_names.add(extension)
+
         url = "https://github.com/orgs/Kaggle/repositories"
-        yield scrapy.Request(url=url, callback=self.parse_account)
+        yield scrapy.Request(url=url, callback=self.parse_account, headers=self.headers)
 
     def parse_account(self, response):
 
         repo_list = response.css('ul.ListView-module__ul--vMLEZ')
         repo_items = repo_list.css('li')
-        #print("Number of repositories: " + str(len(repo_items)))
-
         for repo in repo_items:
             url = f"https://github.com{repo.css('h3 a::attr(href)').get()}"
-            yield scrapy.Request(url=url, callback=self.parse_repo)
+            yield scrapy.Request(url=url, callback=self.parse_repo, headers=self.headers)
 
     def parse_repo(self, response):
         file_list = response.css('table[aria-labelledby="folders-and-files"] tbody tr')
@@ -64,27 +73,38 @@ class GithubSpider(scrapy.Spider):
             is_directory = file.css('svg.octicon-file-directory-fill.icon-directory').get() is not None
             
             if is_directory:
-                yield scrapy.Request(url=full_url, callback=self.parse_repo)
+                yield scrapy.Request(url=full_url, callback=self.parse_repo, headers=self.headers)
             else:
                 if name is not None:
                     filename, extension = os.path.splitext(name)
-                    if self.processed_languages_extensions.get(extension.lower()) is not None:
-                        #if extension == '.tpl':
-                         #   pdb.set_trace()
-                        yield scrapy.Request(url=full_url, callback=self.parse_file, cb_kwargs={'extension': extension})
 
-    def parse_file(self, response, extension):
-        text = response.css('div.Box-sc-g0xbh4-0.bsDwxw.text-mono span::text').get()
-        
+                    if extension.lower() in self.extensions_to_language_names or not extension:
+                        yield scrapy.Request(url=full_url, callback=self.parse_file, cb_kwargs={'filename': filename}, headers=self.headers)
+                        
+
+    def parse_file(self, response, filename):
+
+        text = response.css('div.Box-sc-g0xbh4-0.bsDwxw.text-mono div[data-testid="blob-size"] span::text').get()
         if text:
             matches = re.search(r'\((\d+)\s+loc\)', text)
             if matches:
                 loc = matches.group(1)
-                if extension not in self.processed_languages_extensions_count:
-                    self.processed_languages_extensions_count[extension.lower()] = {'total': int(loc), 'values': [int(loc)]}
-                else:
-                    self.processed_languages_extensions_count[extension.lower()]['total'] += int(loc)
-                    self.processed_languages_extensions_count[extension.lower()]['values'].append(int(loc))
+
+                script = response.css('script[data-target="react-app.embeddedData"]::text').get()
+                data = json.loads(script)
+
+                lang_marker = '"language":"'
+                start = script.find(lang_marker) + len(lang_marker)
+                end = script.find('"', start)
+                
+                language = script[start:end]
+
+                if language in self.valid_language_names:
+                    if language not in self.processed_languages_extensions_count:
+                        self.processed_languages_extensions_count[language] = {'total': int(loc), 'values': [int(loc)]}
+                    else:
+                        self.processed_languages_extensions_count[language]['total'] += int(loc)
+                        self.processed_languages_extensions_count[language]['values'].append(int(loc))
 
     def calculate_median(self, values):
         return statistics.median(values)
@@ -136,12 +156,13 @@ def main():
     def spider_closed(spider):
         print("Processing completed, here are the following results:")
 
-        for extension, data in spider.processed_languages_extensions_count.items():
+        for title, data in spider.processed_languages_extensions_count.items():
+
             median = 0
             if (data['total'] > 0):
                 median = spider.calculate_median(data['values'])
             
-            print(f"Language: {spider.processed_languages_extensions[extension]}, Median: {median}, Total: {data['total']}")
+            print(f"Language: {title}, Median: {median}, Total: {data['total']}")
     
     dispatcher.connect(spider_closed, signal=signals.spider_closed)
 
